1d750165845d0962b001335a779ade64767432d0
Alexander Borovykh <immaculate.pine@gmail.com>
2015-01-15 16:53:19 +0300

    Items list removed from FAQ

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -4,7 +4,6 @@ class FAQController < ApplicationController
   def index
     @page = Page.published.find_by(slug: 'faq')
     @faqs = FAQ.all
-    @items = Item.not_deleted
     @question = Question.new
     @question.init_by_user(current_user)
   end
