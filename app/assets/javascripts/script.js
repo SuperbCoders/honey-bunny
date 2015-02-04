d05e2b544619ad186124822956ece759cfc1f2f0
Alexander Borovykh <immaculate.pine@gmail.com>
2015-02-04 12:24:17 +0100

    Temporary disable W1 payments

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,6 +1,6 @@
 $(function(){
 	$('.navbar-toggle').click(function(){$('body').toggleClass('navbar-wrap');return false;});
 	$('.case-faq .item .capt a').click(function(){$(this).parents('.item').toggleClass('item_open');return false;});
-	$('.switcher label').click(function(){$(this).addClass('current').siblings().removeClass('current');});
+	$('.js-switcher label').click(function(){$(this).addClass('current').siblings().removeClass('current');});
 	if($('.chosen-select').length){$('.chosen-select').chosen({});};
 });
