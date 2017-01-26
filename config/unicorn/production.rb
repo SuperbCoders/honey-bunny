7a0b6569436f8c7767ebed1b5a3da03f0a118599
Oleg Gushul <gushul.o@gmail.com>
2017-01-26 17:04:32 +0300

    Config unicorn fix

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,6 +1,6 @@
 deploy_to  = 'var/www/myhoneybunny.ru'
 rails_root = "#{deploy_to}/current"
-pid_file   = '/shared/tmp/pids/unicorn.pid'
+pid_file   = "#{deploy_to}/shared/tmp/pids/unicorn.pid"
 log_file   = "#{rails_root}/log/unicorn.log"
 err_log    = "#{rails_root}/log/unicorn_error.log"
 old_pid    = pid_file + '.oldbin'
