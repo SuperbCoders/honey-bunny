bac1e332d62b0f7473310fd47b87f4f6a89661c3
Ivan Chernov <chernov.ivan.v@gmail.com>
2017-12-07 18:15:55 +0300

    Fix images

--- app/uploaders/item_main_image_uploader.rb
+++ app/uploaders/item_main_image_uploader.rb
@@ -4,15 +4,15 @@ class ItemMainImageUploader < BaseImageUploader
 
   # Create different versions of your uploaded files:
   version :full do
-    process :resize_to_fill => [360, 760]
+    process :resize_to_fit => [360, 760]
   end
 
   version :thumb do
-    process :resize_to_fill => [200, 420]
+    process :resize_to_fit => [200, 420]
   end
 
   version :thumb_small do
-    process :resize_to_fill => [100, 220]
+    process :resize_to_fit => [100, 220]
   end
 
   version :thumb_square do
